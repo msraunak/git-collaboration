25195b87c17e62415a4ef36a7f56218453aae96d
Mohammad Raunak <raunak@gmail.com>
2018-11-01 06:23:41 -0400

    a new class TTTBoardGUI added

---
+++ buggy_ttt/TTTBoardGUI.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * This class creates the GUI board for TicTacToe
+ * @author M Raunak
+ * @version 1.0 11/1/18
+ */
+ 
+ import javax.swing.*;
+ 
+ public class TTTBoardGUI extends JComponent{
+	 
+	 
+	 
+	 
+ }
